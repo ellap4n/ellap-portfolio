f847ca120bb6873210f350c11b101b2076e2d0c6
ellap4n <epan107@aucklanduni.ac.nz>
2026-01-24 16:51:59 +1300

    preview cards

--- portfolio-react-app/src/App.jsx
+++ portfolio-react-app/src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './Header.jsx';
 import AboutMe from './aboutme.jsx';
 import SkillsSection from './skills.jsx';
+import ProjectCarousel from './projectcarousel.jsx';
+import DownArrow from './downarrow.jsx';
 
 function App() {
 
@@ -11,6 +14,8 @@ function App() {
       <Header /> {/* Website Header! */}
       <AboutMe /> {/* About Me Section */}
       <SkillsSection /> {/* Skills Section */}
+      <h2>Projects</h2>
+      <ProjectCarousel /> {/* Project Carousel Section */}
       <body>
         <p> information here </p>
       </body>
