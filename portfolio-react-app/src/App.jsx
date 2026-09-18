e92ef86a6d2b06c53498442fd209cbc72cba7649
ellap4n <epan107@aucklanduni.ac.nz>
2026-09-19 03:10:41 +1200

    Enabling analytics

--- portfolio-react-app/src/App.jsx
+++ portfolio-react-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import './App.css';
 import Header from './Header.jsx';
 import HomePage from './HomePage.jsx';
@@ -9,7 +10,7 @@ import Footer from './Footer.jsx';
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter>s
       <Header /> {/* Website Header! */}
       <main>
         <Routes>
@@ -20,6 +21,7 @@ function App() {
         </Routes>
       </main>
       <Footer /> {/* Website Footer! */}
+      <Analytics />
     </BrowserRouter >
   );
 }
